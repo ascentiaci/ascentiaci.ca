dfe562dfba5f05b090dda4a6ba1a4e0aa3b8e6c6
Michael Ross <mike@elevareci.ca>
2024-10-08 11:49:59 -0700

    Update sizing and wording of alert

--- src/js/main.js
+++ src/js/main.js
@@ -21,7 +21,9 @@ let alertStartDate = new Date("2024-10-8");
 let alertEndDate = new Date("2024-10-19");
 let content = `
 <div style="background-color: #0033cc; color: #fff; padding: 15px; text-align: center; position: relative; z-index: 1000;">
-  <strong>OPEN HOUSE: Tour, Meet & Win!</strong> 
+  <h1 style="color: #fff;"><strong>OPEN HOUSE: <br>Tour, Meet & Win!</strong> <br>
+  <strong>FULL Scholarship up to the value of $16,300*</strong><br></h1>
+  *Subject to Admissions requirements<br>
   <a href="https://www.facebook.com/share/GxT2SMBWXGgoRYt2/" id="openHouseLink" style="color: #ffcc00; text-decoration: underline;">Click here for more details!</a>
 </div>
 
